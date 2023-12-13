fbf7f8bfcb16a38bc01be1dc9477e6d90981b0d9
legriamax <legriamax@proton.me>
2023-12-13 08:33:06 +0000

    Added some error handling

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SBBML",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v14),
+    ],
+    products: [
+        // Products define the executables and libra
