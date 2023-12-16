ba025ecdaee409bc4e5b36f39caafa8d10d548fd
legriamax <legriamax@proton.me>
2023-12-16 13:26:43 +0000

    Delete plugins.lua

--- SBBML/Services/ObjectDetectionServiceConfiguration.swift
+++ SBBML/Services/ObjectDetectionServiceConfiguration.swift
@@ -42,4 +42,22 @@ public struct ObjectDetectionServiceConfiguration: Equatable {
      - Parameters:
         - confidenceThreshold: The minimum confidence value every detected object needs to match, normalized to [0, 1.0].
         - iouThreshold: The threshold deciding on when two overlapping objects are considered to be the same.
-        - objectDetectionRate: The minimum TimeInterval in seconds between two ObjectDetection requests. Bounding boxes of detected objects can be updated using object tracking between ObjectDetectionrequests, so th
+        - objectDetectionRate: The minimum TimeInterval in seconds between two ObjectDetection requests. Bounding boxes of detected objects can be updated using object tracking between ObjectDetectionrequests, so that the user experience is smooth even when using bigger TimeIntervals. Setting this value to 0 will run ObjectDetection requests as fast-paced as possible.
+        - objectTrackingConfidenceThreshold: The minimum confidence value every tracked object needs to match, normalized to [0, 1.0].
+        - previewVideoGravity: The AVLayerVideoGravity to be used for the CameraStream Preview. This parameter will modify, how the CameraStream Preview is displayed in CameraStreamView.
+        - computeUnits: The MLComputeUnits on which the ObjectDetection will run on.
+        - distanceRecordingEnabled: A Boolean value indicating whether the distance of every DetectedObject should be calculated. Note that not every device does support this (at least 2 back-facing cameras are required). Might delay object detection by almost 1 second.
+        - detectableClassLabels: A list containing all class labels that should be detected (and are supported by the provided CoreML model). If nil, all labels will be detected and no filtering will be applied.
+     */
+    public init(confidenceThreshold: VNConfidence = 0.5, iouThreshold: Double = 0.6, objectDetectionRate: TimeInterval = 0.0, objectTrackingEnabled: Bool = false, objectTrackingConfidenceThreshold: VNConfidence = 0.3, previewVideoGravity: AVLayerVideoGravity = .resizeAspectFill, computeUnits: MLComputeUnits = .all, distanceRecordingEnabled: Bool = false, detectableClassLabels: [String]? = nil) {
+        self.confidenceThreshold = confidenceThreshold
+        self.iouThreshold = iouThreshold
+        self.objectDetectionRate = objectDetectionRate
+        self.objectTrackingEnabled = objectTrackingEnabled
+        self.objectTrackingConfidenceThreshold = objectTrackingConfidenceThreshold
+        self.previewVideoGravity = previewVideoGravity
+        self.computeUnits = computeUnits
+        self.distanceRecordingEnabled = distanceRecordingEnabled
+        self.detectableClassLabels = detectableClassLabels
+    }
+}
