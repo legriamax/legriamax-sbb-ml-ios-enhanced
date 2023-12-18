39f2dcbaca87ad010dacc888048290e099eabd50
legriamax <legriamax@proton.me>
2023-12-18 19:17:02 +0000

    Add .travis.yml file

--- SBBML/Services/internal/Camera/CameraController.swift
+++ SBBML/Services/internal/Camera/CameraController.swift
@@ -79,4 +79,78 @@ class CameraController: NSObject, CameraControllerProtocol {
         self.depthRecordingEnabled = depthRecordingEnabled
         DispatchQueue(label: "prepare").async {
             guard AVCaptureDevice.authorizationStatus(for: .video) != .denied else {
+                self.errorSubject.send(.deniedCameraAuthorization)
+                return
+            }
+            
+            guard self.captureSession.outputs.isEmpty && self.captureSession.inputs.isEmpty else {
+                // return, since configure has already been called
+                return
+            }
+            
+            do {
+                let camera = try self.configureCaptureDevices(depthRecordingEnabled: depthRecordingEnabled)
+                try self.configureDeviceInputs(for: camera)
+                try self.configureDeviceOutputs(depthRecordingEnabled: depthRecordingEnabled)
+            } catch let error as ObjectDetectionError {
+                self.errorSubject.send(error)
+            } catch {
+                Logger.log("Unknown error while trying to configure CameraController: \(error)", .error)
+            }
+        }
+    }
+    
+    private func configureCaptureDevices(depthRecordingEnabled: Bool) throws -> AVCaptureDevice {
+        guard let camera = availableCaptureDevice(depthRecordingEnabled: depthRecordingEnabled) else {
+            throw ObjectDetectionError.noCamerasAvailable
+        }
         
+        try camera.lockForConfiguration()
+        if camera.deviceType == .builtInDualWideCamera {
+            // Search for highest resolution with half-point depth values
+            let depthFormats = camera.activeFormat.supportedDepthDataFormats
+            let filtered = depthFormats.filter({
+                CMFormatDescriptionGetMediaSubType($0.formatDescription) == kCVPixelFormatType_DepthFloat16
+            })
+            let selectedFormat = filtered.max(by: {
+                first, second in CMVideoFormatDescriptionGetDimensions(first.formatDescription).width < CMVideoFormatDescriptionGetDimensions(second.formatDescription).width
+            })
+            camera.activeDepthDataFormat = selectedFormat
+        }
+        camera.unlockForConfiguration()
+        
+        return camera
+    }
+    
+    private func availableCaptureDevice(depthRecordingEnabled: Bool) -> AVCaptureDevice? {
+        if depthRecordingEnabled, let device = AVCaptureDevice.default(.builtInDualWideCamera, for: .video, position: .back) {
+            currentDeviceSupportsDepthRecording = true
+            return device
+        } else if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        } else {
+            return nil
+        }
+    }
+    
+    private func configureDeviceInputs(for camera: AVCaptureDevice) throws {
+        let cameraInput = try AVCaptureDeviceInput(device: camera)
+        
+        captureSession.beginConfiguration()
+        captureSession.sessionPreset = .vga640x480 // higher resolution is critical because it will lead to buffers being dropped
+        
+        if captureSession.canAddInput(cameraInput) {
+            captureSession.addInput(cameraInput)
+        } else {
+            captureSession.commitConfiguration()
+            throw ObjectDetectionError.inputsAreInvalid
+        }
+        
+        captureSession.commitConfiguration()
+    }
+    
+    private func configureDeviceOutputs(depthRecordingEnabled: Bool) throws {
+        if captureSession.canAddOutput(videoDataOutput) {
+            captureSession.addOutput(videoDataOutput)
+            
+            videoDataOutput.alwaysDiscardsLateVideoFrames = tr
